14bd8a42a93edc4b8a1f55cbe2828acf24ef0d35
RojuroIsNotHere <kvalu003@fiu.edu>
2024-09-28 23:00:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def calcRentEstimate(rental_type, est_prop_val, desired_coverage):
             rate = 0.06
         if rental_type == "House":
             rate = 0.09
-        return estimate = rate * desired_coverage * ( est_prop_val / 0.6 ) 
+        return rate * desired_coverage * ( est_prop_val / 0.6 ) 
 
 
 # Home page
