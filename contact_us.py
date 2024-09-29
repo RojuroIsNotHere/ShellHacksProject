2bfb917f8920633bd1727a80a869c407c91f04af
RojuroIsNotHere <kvalu003@fiu.edu>
2024-09-29 09:28:06 -0400

    Update contact_us.py

--- contact_us.py
+++ contact_us.py
@@ -1,24 +1,4 @@
 import streamlit as st
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.header import Header
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-
-def send_email(sender, password, receiver, smtp_server, smtp_port, email_message, subject):
-  message = MIMEMultipart()
-  message['To'] = Header(receiver)
-  message['From'] = Header(sender)
-  message['Subject'] = Header(subject)
-  message.attach(MIMEText(email_message), 'plain', 'utf-8')
-
-  server = smtplib.SMTP(smtp_server, smtp_port)
-  server.starttls()
-  server.ehlo()
-  server.login(sender, password)
-  text = message.as_string()
-  server.sendemail(sender, reciever, text)
-  server.quit()
 
 def display():
     
@@ -34,17 +14,4 @@ def display():
 
     if submit_res:
 
-      extra_info = """
-      -----------------------------------------------
-
-      Email Address of sender {} \n
-
-      Sender Full Name {} \n
-
-      ----------------------------------------------- \n \n
-
-      """.format(email, fullName)
-
-      message = extra_info + text
-    
-      send_email("quotematescom@gmail", "#123098Wow", email, "smtp.gmail.com", 587, message, "Test Email for hackathon")
+      st.success("Email sent!")
