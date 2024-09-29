a50db95c00ca4d2a8d3d58732348b616589f9652
RojuroIsNotHere <kvalu003@fiu.edu>
2024-09-29 07:39:48 -0400

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -61,6 +61,6 @@ def calcRentEstimate(rental_type, est_prop_val, desired_coverage, quote_frequenc
     elif rental_type == "House":
         rate = 0.03
     if quote_frequency == "Monthly":
-        return ( rate * desired_coverage + (est_prop_val * 0.5) ) / 12
+        return ( rate * desired_coverage + (est_prop_val * 0.01) ) / 12
     else:
-        return rate * desired_coverage + (est_prop_val * 0.5)
+        return rate * desired_coverage + (est_prop_val * 0.01)
