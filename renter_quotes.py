e618384889be5c78504704dc4cc5a508735100d6
RojuroIsNotHere <kvalu003@fiu.edu>
2024-09-29 07:37:58 -0400

    Update renter_quotes.py

--- renter_quotes.py
+++ renter_quotes.py
@@ -8,8 +8,9 @@ def display():
     rental_type = st.selectbox("Rental Type", ["Apartment", "Condo", "Townhouse", "House"])
     est_prop_val = st.number_input("Estimated Value of Personal Property", min_value=1000)
     desired_coverage = st.number_input("Desired Liability Coverage", min_value=1000)
+    quote_frequency = st.selectbox("Select quote frequency:", ["Monthly", "Yearly"])
 
     if st.button("Submit"):
-        estimate = calcRentEstimate(rental_type, est_prop_val, desired_coverage)
+        estimate = calcRentEstimate(rental_type, est_prop_val, desired_coverage, quote_frequency)
         st.success(f"Renters Insurance Quote: ${estimate:.2f}")
         st.balloons()
