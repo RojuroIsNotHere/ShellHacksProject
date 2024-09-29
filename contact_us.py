50440570f8ca3efd950e2ea559b722cf97edda14
RojuroIsNotHere <kvalu003@fiu.edu>
2024-09-29 08:32:04 -0400

    Update contact_us.py

--- contact_us.py
+++ contact_us.py
@@ -4,8 +4,13 @@ def send_email(message):
   st.write(message)
 
 def display():
-    
 
-    if st.button("Submit"):
-        
-        st.balloons()
+  message = "I am coming from Streamlit"
+    
+  with st.form("Email Form"):
+    fullName = st.text_input(label="Full Name", placeholder = "Please enter your full name")
+    email = st.text_input(label="Email Address", placeholder = "Please enter your email address")
+    text = st.text_input(label="Comments", placeholder = "Please enter your comments")
+    submit_res = st.form_submit_button(label="Send")
+
+  send_email(message)
